bde6169ff7fc804feb9df34c1cbd65aaf0a287bf
yunior1250 <yunior.garcia.padilla@gmail.com>
2023-12-05 12:28:47 -0400

    modelo1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,52 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@argon.com',
             'password' => bcrypt('secret')
         ]);
+         /* Colegios  */
+         DB::table('colegios')->insert([
+            'nombre' => 'Colegio 1',
+            'direccion' => 'Direccion 1',
+            'telefono' => '123456789',
+            'email' => 'vanacio@gmial.com',
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c'
+        ]);
+
+        DB::table('colegios')->insert([
+            'nombre' => 'Colegio 2',
+            'direccion' => 'Direccion 2',
+            'telefono' => '123456789',
+            'email' => 'salle@gmail.com',
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c'
+        ]);
+
+        DB::table('colegios')->insert([
+            'nombre' => 'Colegio 3',
+            'direccion' => 'Direccion 3',
+            'telefono' => '123456789',
+            'email' => 'walverto@gmail.com',
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c'
+        ]);
+
+
+        /* Aulas  */
+        DB::table('aulas')->insert([
+            'nombre' => 'Aula 1',
+            'capacidad' => 30,
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c',
+            'colegio_id' => 1,
+        ]);
+
+        DB::table('aulas')->insert([
+            'nombre' => 'Aula 2',
+            'capacidad' => 30,
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c',
+            'colegio_id' => 1,
+        ]);
+
+        DB::table('aulas')->insert([
+            'nombre' => 'Aula 3',
+            'capacidad' => 30,
+            'foto' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldiario.es%2Fsociedad%2Fcolegios-privados-publicos-escuelas-c',
+            'colegio_id' => 2
+        ]);
     }
 }
--- resources/views/aulas/edit.blade.php
+++ resources/views/aulas/edit.blade.php
@@ -111,8 +111,8 @@
             </div>
     <div class="row">
                 <div class="col-md-12">
-                    <h2>Video de la Cámara</h2>
-                    <video id="videoElement" width="640" height="480" autoplay></video>
+                    <h3>Captura Video Pelea</h3>
+                    <video id="videoElement" width="400" height="300" autoplay></video>
                 </div>
             </div> 
             <div class="card-body">
@@ -164,24 +164,24 @@
                     <button type="submit" class="btn btn-primary">Actualizar Aula</button>
                 </form>
 
-                <!-- Copyright 2023 The MediaPipe Authors.
-
-        Licensed under the Apache License, Version 2.0 (the "License");
-        you may not use this file except in compliance with the License.
-        You may obtain a copy of the License at
-
-             http://www.apache.org/licenses/LICENSE-2.0
-
-        Unless required by applicable law or agreed to in writing, software
-        distributed under the License is distributed on an "AS IS" BASIS,
-        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        See the License for the specific language governing permissions and
-        limitations under the License. -->
-
+             
                
             </div>
         </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Acceder a la cámara del usuario
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function(stream) {
+                    var video = document.getElementById('videoElement');
+                    video.srcObject = stream;
+                })
+                .catch(function(error) {
+                    console.log('Error al acceder a la cámara: ', error);
+                });
+        });
+    </script>
 @endsection
 
 
